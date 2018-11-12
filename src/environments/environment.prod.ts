3ab12589f6ccd3dbda4af3392447a114fa58c477
MetroXVN <40994962+MetroXVN@users.noreply.github.com>
2018-11-12 14:30:48 +0200

    all files before prodbuild

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDA3F3mp-ZWDYZVMPW-G0CyYsJqmPD7DzM",
+    authDomain: "clientpanelprod-f7da8.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-f7da8.firebaseio.com",
+    projectId: "clientpanelprod-f7da8",
+    storageBucket: "clientpanelprod-f7da8.appspot.com",
+    messagingSenderId: "964248156363"
+  }
+};
